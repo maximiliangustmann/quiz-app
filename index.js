b82269a92e445835f03b1b6856adfe5ec62a5983
maximiliangustmann <maxigustmann@wtnet.de>
2020-07-22 15:39:40 +0200

    Interactive Headline

--- index.js
+++ index.js
@@ -8,27 +8,32 @@ var buttonIndex = document.querySelector('.button__index');
 var buttonBookmarks = document.querySelector('.button__bookmarks');
 var buttonCreate = document.querySelector('.button__create');
 var buttonProfile = document.querySelector('.button__profile');
+var headline = document.querySelector('h1');
 buttonIndex.addEventListener('click', function () {
   bookmarks.classList.add('hide');
   create.classList.add('hide');
   profile.classList.add('hide');
   index.classList.remove('hide');
+  headline.textContent = 'Home';
 });
 buttonBookmarks.addEventListener('click', function () {
   index.classList.add('hide');
   create.classList.add('hide');
   profile.classList.add('hide');
   bookmarks.classList.remove('hide');
+  headline.textContent = 'Bookmarks';
 });
 buttonCreate.addEventListener('click', function () {
   bookmarks.classList.add('hide');
   index.classList.add('hide');
   profile.classList.add('hide');
   create.classList.remove('hide');
+  headline.textContent = 'Create';
 });
 buttonProfile.addEventListener('click', function () {
   bookmarks.classList.add('hide');
   create.classList.add('hide');
   index.classList.add('hide');
   profile.classList.remove('hide');
+  headline.textContent = 'Profile';
 });
--- src/js/index.js
+++ src/js/index.js
@@ -6,12 +6,14 @@ const buttonIndex = document.querySelector('.button__index')
 const buttonBookmarks = document.querySelector('.button__bookmarks')
 const buttonCreate = document.querySelector('.button__create')
 const buttonProfile = document.querySelector('.button__profile')
+const headline = document.querySelector('h1')
 
 buttonIndex.addEventListener('click', () => {
   bookmarks.classList.add('hide')
   create.classList.add('hide')
   profile.classList.add('hide')
   index.classList.remove('hide')
+  headline.textContent = 'Home'
 })
 
 buttonBookmarks.addEventListener('click', () => {
@@ -19,6 +21,7 @@ buttonBookmarks.addEventListener('click', () => {
   create.classList.add('hide')
   profile.classList.add('hide')
   bookmarks.classList.remove('hide')
+  headline.textContent = 'Bookmarks'
 })
 
 buttonCreate.addEventListener('click', () => {
@@ -26,6 +29,7 @@ buttonCreate.addEventListener('click', () => {
   index.classList.add('hide')
   profile.classList.add('hide')
   create.classList.remove('hide')
+  headline.textContent = 'Create'
 })
 
 buttonProfile.addEventListener('click', () => {
@@ -33,4 +37,5 @@ buttonProfile.addEventListener('click', () => {
   create.classList.add('hide')
   index.classList.add('hide')
   profile.classList.remove('hide')
+  headline.textContent = 'Profile'
 })
